1a6fbfa98802b581b43fac575c6655c1856a46f0
Moses The Master <moritztiedje@gmail.com>
2018-07-24 19:29:25 +0200

    Change nav menu content

--- src/app/redux/active-content.ts
+++ src/app/redux/active-content.ts
@@ -2,8 +2,12 @@ import { ActionReducer, Action } from '@ngrx/store';
 import { NavigateContentAction } from './actions';
 
 export const VIDEO = 'VIDEO';
+export const PROBLEM = 'PROBLEM';
+export const SOLUTION = 'SOLUTION';
+export const PARTNERS = 'PARTNERS';
 export const DONATE_PAGE = 'DONATE PAGE';
 export const GALLERY = 'GALLERY';
+export const CONTACT = 'CONTACT';
 
 export interface PageState {
   activeContent: string;
--- src/app/top-nav-bar/navigation-menu/navigation-menu.component.ts
+++ src/app/top-nav-bar/navigation-menu/navigation-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { NavigateContentAction } from '../../redux/actions';
-import { PageState, VIDEO, DONATE_PAGE, GALLERY } from 'src/app/redux/active-content';
+import { PageState, VIDEO, DONATE_PAGE, GALLERY, PROBLEM, SOLUTION, PARTNERS, CONTACT } from 'src/app/redux/active-content';
 
 @Component({
   selector: 'app-navigation-menu',
@@ -29,12 +29,28 @@ export class NavigationMenuComponent {
     return this.selection === VIDEO;
   }
 
-  selectDonate() {
-    this.store.dispatch(new NavigateContentAction(DONATE_PAGE))
+  selectProblem() {
+    this.store.dispatch(new NavigateContentAction(PROBLEM))
   }
 
-  donateIsSelected() {
-    return this.selection === DONATE_PAGE;
+  problemIsSelected() {
+    return this.selection === PROBLEM;
+  }
+
+  selectSolution() {
+    this.store.dispatch(new NavigateContentAction(SOLUTION))
+  }
+
+  solutionIsSelected() {
+    return this.selection === SOLUTION;
+  }
+
+  selectPartners() {
+    this.store.dispatch(new NavigateContentAction(PARTNERS))
+  }
+
+  partnersIsSelected() {
+    return this.selection === PARTNERS;
   }
 
   selectGallery() {
@@ -43,6 +59,22 @@ export class NavigationMenuComponent {
 
   galleryIsSelected() {
     return this.selection === GALLERY;
+  }
+
+  selectDonate() {
+    this.store.dispatch(new NavigateContentAction(DONATE_PAGE))
+  }
+
+  donateIsSelected() {
+    return this.selection === DONATE_PAGE;
+  }
+
+  selectContact() {
+    this.store.dispatch(new NavigateContentAction(CONTACT))
+  }
+
+  contactIsSelected() {
+    return this.selection === CONTACT;
   }
 
 }

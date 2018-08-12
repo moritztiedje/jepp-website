2b3c576c4afd98546a835730926ba5d4b4d01ddb
Moritz Tiedje <moritztiedje@gmail.com>
2018-08-12 18:36:18 +0200

    Add logos and links of cooperators

--- src/app/content-section/partners/partners.component.ts
+++ src/app/content-section/partners/partners.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { WindowSizes } from 'src/app/redux/window-size';
+import { Store } from '@ngrx/store';
+import { PageState } from 'src/app/app.module';
 
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
   styleUrls: ['./partners.component.scss']
 })
-export class PartnersComponent implements OnInit {
+export class PartnersComponent {
 
-  constructor() { }
+  windowSize: WindowSizes;
 
-  ngOnInit() {
+  constructor(store: Store<PageState>) {
+    store.subscribe(pageState => this.windowSize = pageState.windowSize);
+  }
+
+  displayMode() {
+    if (this.windowSize === WindowSizes.Mobile)
+      return 'mobile';
+    else
+      return 'desktop';
   }
 
 }
